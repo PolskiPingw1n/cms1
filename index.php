d37319159b5550221bef23cc66479a0ce9ec63d6
PolskiPingw1n <mk-kurek@wp.pl>
2024-05-20 12:31:07 +0200

    "Added Bootstrap CSS, navigation bar, and refactored database connection and post display logic to use classes"

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
+<?php
+require_once("class/User.class.php");
+require_once("class/Post.class.php");
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CMS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+        crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -11,32 +19,54 @@
         <h1>Nagłowek strony</h1>
     </header>
     <div id="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between px-3">
+            <a class="navbar-brand">Nawigacja</a>
+            <?php if(User::isLogged()) : ?>
+                <!-- zalogowany -->
+                <a href="profile.php">
+                    <button class="btn btn-primary">
+                        <i class="fa-solid fa-user"></i> Profil
+                    </button>
+                </a>
+            <?php else: ?>
+                <!-- nie zalogowany -->
+                <a href="login.php">
+                    <button class="btn btn-primary">
+                        <i class="fa-solid fa-user"></i> Zaloguj się
+                    </button>
+                </a>
+            <?php endif; ?>  
+        </nav>
         <?php
-        //nowe połaczenie do bazy danych
-        $db = new mysqli('localhost', 'root', '', 'suprredit');
-        //przygotuj kwerendę
-        $q = $db->prepare("SELECT post.id, post.imgUrl, post.title, 
-                                    post.timestamp, user.email 
-                            FROM `post` 
-                            INNER JOIN user ON post.author = user.ID
-                            ORDER BY post.timestamp DESC;");
-        //wywołaj kwerendę
-        $q->execute();
-        //pobierz dane
-        $result = $q->get_result();
-        while($row = $result->fetch_assoc()) {
-            //$row to jeden wiersz z bazy danych
+        $postList = Post::GetPosts();
+        foreach ($postList as $post) {
+            //iterujemy przez tablicę postów
             echo '<div class="post-block">';
-            echo '<h2 class="post-title">'.$row['title'].'</h3>';
-            echo '<h3 class="post-author">'.$row['email'].'</h6>';
-            echo '<img src="'.$row['imgUrl'].'" alt="obrazek posta" class="post-image">';
-            echo '<p class="post-description">TODO: Opis posta</p>';
+            echo '<h2 class="post-title">'.$post->GetTitle().'</h3>';
+            echo '<h3 class="post-author">'.$post->GetAuthor().'</h6>';
+            echo '<h3 class="post-author">'.$post->GetAuthorEmail().'</h6>';
+            echo '<img src="'.$post->GetImageURL().'" alt="obrazek posta" class="post-image">';
             echo '<div class="post-footer">
-                <span class="post-meta">'.$row['timestamp'].'</span>
+                <span class="post-meta">'.$post->GetTimestamp().'</span>
                 <span class="post-score">TODO: punkty</span>
                 </div>';
             echo '</div>'; //post-block
         }
+
+
+        // while($row = $result->fetch_assoc()) {
+        //     //$row to jeden wiersz z bazy danych
+        //     echo '<div class="post-block">';
+        //     echo '<h2 class="post-title">'.$row['title'].'</h3>';
+        //     echo '<h3 class="post-author">'.$row['email'].'</h6>';
+        //     echo '<img src="'.$row['imgUrl'].'" alt="obrazek posta" class="post-image">';
+        //     echo '<p class="post-description">TODO: Opis posta</p>';
+        //     echo '<div class="post-footer">
+        //         <span class="post-meta">'.$row['timestamp'].'</span>
+        //         <span class="post-score">TODO: punkty</span>
+        //         </div>';
+        //     echo '</div>'; //post-block
+        // }
         ?>
 
         <!--<div class="post-block">
@@ -52,5 +82,6 @@
         </div>
     -->
     </div>
+    <script src="https://kit.fontawesome.com/4f765d06bb.js" crossorigin="anonymous"></script>
 </body>
 </html>
